48b800d6a50f1cbb50f5fb5a1ac317860a664384
Carlos Villatoro <crvb0797@gmai.com>
2020-05-05 17:58:40 -0600

    Modal de click listo, falta traer resultados

--- javascript/api.js
+++ javascript/api.js
@@ -1,23 +1,22 @@
-(async function load(){
-    async function getData(url){
-        const response = await fetch(url)
-        const data = await response.json()
-        return data;
-    }
-    
+(async function load() {
+  async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
 
-        const $form = document.querySelector('#form');
-        $form.addEventListener('submit', function (event){
-            event.preventDefault();
-        })
-       
-        const character = await getData(`https://rickandmortyapi.com/api/character/`)
-        const character2 = await getData(`https://rickandmortyapi.com/api/character/?page=2`)
-       
-       
-       
-    function characterItemsTemplate(character){
-        return(`<div class="content-character">
+  const $form = document.querySelector("#form");
+  $form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+
+  const character = await getData(`https://rickandmortyapi.com/api/character/`);
+  const character2 = await getData(
+    `https://rickandmortyapi.com/api/character/?page=2`
+  );
+
+  function characterItemsTemplate(character) {
+    return `<div class="content-character">
         <div class="caja">
             <div class="image">
                 <img src="${character.image}" alt="">
@@ -31,36 +30,47 @@
                 <p><span>Genero:</span>${character.gender}</p>
             </div>
         </div>
-    </div>`)
-    }
+    </div>`;
+  }
 
+  function createTemplate(HTMLString) {
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0];
+  }
 
-    function addEventClick($element){
-        $element.addEventListener('click', () => {
-            alert("Hola soy un click")
-        })
-    }
+  const $overlay = document.getElementById("overlay");
+  const $modal = document.getElementById("modal");
+  const $hideModal = document.getElementById("hide-modal");
 
-    function createTemplate(HTMLString){
-        const html = document.implementation.createHTMLDocument();
-        html.body.innerHTML = HTMLString
-        return html.body.children[0]
-    }
-    
-    function renderCharter(elemnto, container){
-        elemnto.results.forEach(character => {
-            const HTMLString = characterItemsTemplate(character);
-            const characterElement = createTemplate(HTMLString)
-            container.before(characterElement);
-            addEventClick(characterElement)
-        });
-    }
+  function showModal() {
+    $overlay.classList.add("active");
+    $modal.style.animation = "modalIn .8s forwards";
+  }
 
-    const $container_persons = document.querySelector("#container_persons h2");
-    renderCharter(character , $container_persons)
+  $hideModal.addEventListener("click", () => {
+    $overlay.classList.remove("active");
+    $modal.style.animation = "modalOut .8s forwards";
+  });
 
-    const $container_persons2 = document.querySelector("#container_persons h2");
-    renderCharter(character2 , $container_persons2)
+  function addEventClick($element) {
+    $element.addEventListener("click", () => {
+      showModal();
+    });
+  }
 
+  function renderCharter(elemnto, container) {
+    elemnto.results.forEach((character) => {
+      const HTMLString = characterItemsTemplate(character);
+      const characterElement = createTemplate(HTMLString);
+      container.before(characterElement);
+      addEventClick(characterElement);
+    });
+  }
 
+  const $container_persons = document.querySelector("#container_persons h2");
+  renderCharter(character, $container_persons);
+
+  const $container_persons2 = document.querySelector("#container_persons h2");
+  renderCharter(character2, $container_persons2);
 })();
